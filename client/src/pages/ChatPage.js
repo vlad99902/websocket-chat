aea66225631614f37f8cb953789caaeefd02e05f
vlad99902 <legkovv@mail.ru>
2021-02-25 08:53:32 +0300

    layout: chat page

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import styled from 'styled-components';
+import { Container } from '../utils/primaryStyledComponents';
+import { ChatContainer } from '../containers/ChatContainer';
 
 export const ChatPage = () => {
   return (
-    <div>
-      <h1>Chat page</h1>
-    </div>
+    <ChatPageWrapper>
+      <Container>
+        <ChatContainer />
+      </Container>
+    </ChatPageWrapper>
   );
 };
+
+const ChatPageWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
