c338ba13a95e4e9596bfe7291ac4470811be0682
vlad99902 <legkovv@mail.ru>
2021-02-25 06:27:31 +0300

    layout: button component

---
+++ client/src/components/BaseButton.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { colors } from '../utils/colors';
+
+export const BaseButton = ({ children, ...rest }) => {
+  return <StyledButton {...rest}>{children}</StyledButton>;
+};
+
+const StyledButton = styled.button`
+  background-color: ${colors.$pink};
+  margin-top: ${(props) => props.mt};
+  margin-left: ${(props) => props.ml};
+
+  color: ${colors.$white};
+  cursor: pointer;
+  user-select: none;
+  border: 1px solid ${colors.$pink};
+  font-family: 'Jetbrains mono';
+  padding: 10px 15px;
+  border-radius: 18px;
+  font-size: 1rem;
+
+  :focus {
+    border: 1px solid ${colors.$black};
+  }
+`;
