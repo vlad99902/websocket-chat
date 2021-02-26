8cca80ef3fb3d8c22f177c20924e66114eda9c61
vlad99902 <legkovv@mail.ru>
2021-02-26 09:13:51 +0300

    serverside socket io added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,73 @@
 const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const app = express();
+app.use(cors);
 
 const PORT = process.env.PORT || 5000;
 
 const server = require('http').createServer(app);
+
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    //error from add user function
+    //callback from front if error
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    //send message to everyone in room
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+    //add to room
+    socket.join(user.room);
+
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    //callback from front
+    callback();
+    console.log('user connected', user);
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', { room: user.room, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'admin',
+        text: `${user.name} has left`,
+      });
+    }
+
+    console.log('User had left!');
+  });
+});
 
 app.get('/', (req, res) =>
   res.send('Server is still running, but route not found'),
